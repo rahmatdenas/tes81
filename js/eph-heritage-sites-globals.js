aeedfffec7581f7e20206cafd13b17645202f1a8
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2026-07-11 11:52:48 +0700

    Update eph-heritage-sites-globals.js

--- js/eph-heritage-sites-globals.js
+++ js/eph-heritage-sites-globals.js
@@ -86,10 +86,17 @@ WHERE {
   {
     SELECT DISTINCT ?s ?p ?l WHERE {
       VALUES ?j { <PLACEHOLDER_JENIS> }
-      BIND(<PLACEHOLDER_NEGARA> AS ?p)
+      
+      # 1. WAJIB: Cari entitas yang negaranya adalah terpilih, dan catat lokasi spesifiknya di ?l
       ?s wdt:P31 ?j ;
+         wdt:P17 <PLACEHOLDER_NEGARA> ;
          wdt:<PLACEHOLDER_PROP_LOKASI> ?l .
-      ?l wdt:P131* ?p .
+         
+      # 2. OPSIONAL: Coba cari ?p (wilayah 1 tingkat di bawah negara) yang menaungi ?l
+      OPTIONAL {
+        ?p wdt:P131 <PLACEHOLDER_NEGARA> . 
+        ?l wdt:P131* ?p .
+      }
     }
     ORDER BY ?s ?p ?l
     <PLACEHOLDER_LIMIT_OFFSET>
@@ -103,7 +110,7 @@ WHERE {
   BIND(SUBSTR(STR(?s), 32) AS ?SQ) .
   BIND(SUBSTR(STR(?p), 32) AS ?PQ) .
   BIND(SUBSTR(STR(?l), 32) AS ?LQ)
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "id". }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "id,en". }
 }`
 };
 
